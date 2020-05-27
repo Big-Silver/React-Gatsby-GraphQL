78b92f7e69a699c334bdb8c93181ee96d38ebd64
big-silver <sergioreisbon@gmail.com>
2020-05-27 04:21:13 -0400

    add gatsby-graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,31 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+const fs = require(`fs`)
+const fetch = require(`node-fetch`)
+const { buildClientSchema } = require(`graphql`)
+const { createHttpLink } = require(`apollo-link-http`)
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-graphql`,
+      options: {
+        fieldName: `github`,
+        typeName: `GitHub`,
+        createLink: () =>
+          createHttpLink({
+            uri: `https://api.github.com/graphql`,
+            headers: {
+              Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
+            },
+            fetch,
+          }),
+        createSchema: async () => {
+          const json = JSON.parse(fs.readFileSync(`${__dirname}/github.json`))
+          return buildClientSchema(json.data)
+        },
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,32 @@
 import React from "react"
+import { graphql } from "gatsby"
+import RepositoryList from "../components/RepositoryList"
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+const Index = ({ data }) => (
+  <div>
+    <h1>My repositories</h1>
+    <RepositoryList repositories={data.github.viewer.repositories} />
+  </div>
+)
+
+export default Index
+
+export const query = graphql`
+  query RepositoriesQuery {
+    github {
+      viewer {
+        repositories(
+          privacy: PUBLIC
+          affiliations: OWNER
+          isFork: false
+          first: 100
+        ) {
+          nodes {
+            name
+            url
+          }
+        }
+      }
+    }
+  }
+`
